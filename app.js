40b6769e07caf689c326a72117ffb2b9209e1cd1
Adam Wheatley <adam.wheatley@caddis.co>
2018-03-06 13:13:22 -0600

    Add toggleSelected and updateSliderVal functions

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const numbers = '1234567890';
 const symbols = '`~!@#$%^&*()-_=+,<.>/?;:[{]}';
-const charNum = document.querySelector('.form__characters');
-const nums = document.querySelector('.numbers');
-const syms = document.querySelector('.symbols');
+const charNum = document.querySelector('.-characters');
+const nums = document.querySelector('.-numbers');
+const syms = document.querySelector('.-symbols');
 const passwordDiv = document.querySelector('.password');
 const btn = document.querySelector('.form__button');
-
-btn.addEventListener('click', (e) => {
-	let charStr = letters;
-
-	nums.checked ? chars += numbers : '';
-	syms.checked ? chars += symbols : '';
-	passwordDiv.textContent = generatePassword(charNum.value, chars);
-
-	e.preventDefault();
-});
+const checkLabel = document.querySelectorAll('.form__label.-checkbox');
+const charCount = document.querySelector('.form__characters');
 
 function generatePassword(num, chars) {
 	let password = '';
@@ -27,8 +19,34 @@ function generatePassword(num, chars) {
 	return password;
 }
 
+function toggleSelected(el) {
+	el.classList.contains('-selected') ?
+	el.classList.remove('-selected') :
+	el.classList.add('-selected');
+}
 
+function updateSliderVal(value) {
+	charCount.textContent = value;
+}
 
+btn.addEventListener('click', (e) => {
+	let charStr = letters;
 
+	nums.checked ? charStr += numbers : '';
+	syms.checked ? charStr += symbols : '';
+	passwordDiv.textContent = generatePassword(charNum.value, charStr);
 
+	e.preventDefault();
+});
 
+checkLabel.forEach((el) => {
+	el.addEventListener('click', () => {
+		toggleSelected(el);
+	})
+});
+
+charNum.addEventListener('input', () => {
+	let val = charNum.value;
+
+	updateSliderVal(val);
+});
